533ce2dd3bbf4cb1a8c07b4e2a23b9c9c97967d8
KaramPark <com3892@gmail.com>
2025-06-25 15:30:37 +0900

    -

--- src/main/java/com/pkr/firstProject/controller/DemoController.java
+++ src/main/java/com/pkr/firstProject/controller/DemoController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DemoController {
-    @GetMapping("/hello")
+    @GetMapping("/")
     public String hello() {
         return "hello";
     }
